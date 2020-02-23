a35f41245d90384e29bf07be89829aef0bf33b05
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-02-23 12:20:15 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/DescribeAModuleBadly.pm
@@ -0,0 +1,24 @@
+package Acme::CPANModules::DescribeAModuleBadly;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our $LIST = {
+    summary => 'The Acme::CPANModules::DescribeAModuleBadly namespace',
+    description => <<'_',
+
+Acme::CPANModules::DescribeAModuleBadly::* modules should contain lists of
+modules that are being described badly. Inspired by Jimmy Fallon's Twitter
+hashtag #DescribeAMovieBadly (Feb 4, 2020), the idea is to give an accurate
+description of a certain element or aspect of the module but somehow miss the
+whole point of it.
+
+_
+    entries => [
+    ],
+};
+
+1;
+# ABSTRACT:
